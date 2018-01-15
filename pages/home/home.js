ab1842749d89de1d630836b19a76c74ce1416e53
heweinan <870603569@qq.com>
2018-01-15 16:37:04 +0800

    完成初步界面

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,14 +5,48 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    typeArray:['新建','吃喝','其他'],
+    sumbitBtnCss:'submit-view',
+    sumbitBtnMsg:'提交',
+    index: 0
+    
   },
-
+  bindPickerChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value
+    })
+  },
+  submitClick: function(){
+    this.setData({
+      sumbitBtnCss : 'submit-view submit-view-click',
+      sumbitBtnMsg : '提交中...'
+    });
+    this.httprequst();
+    
+  },
+  /**
+   * 网络请求
+   */
+  httprequst : function(){
+    var instanceApp = getApp();
+    wx.request({
+      url: instanceApp.data.path, //仅为示例，并非真实的接口地址
+      data: {
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        console.log(res.data)
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    
   },
 
   /**
