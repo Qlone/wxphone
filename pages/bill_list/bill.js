1e8cdf28ff153834e795a40f925ca69be4c9202b
heweinan <870603569@qq.com>
2018-01-18 15:10:48 +0800

    完成数据提交

--- pages/bill_list/bill.js
+++ pages/bill_list/bill.js
@@ -8,25 +8,46 @@ Page({
     item_distance : 20,
     touch :{
       startX : 0,
-      startX : 0
+      startX : 0,
+      times : 1.0
     }
   },
 
   handletouchmove: function (event) {
-    var i = this.data.item_distance +1;
+    var b = this.data.touch.times;
+    var isPull = (event.touches[0].pageY > this.data.touch.startY) ? 1 : 0;
+    var x = this.data.touch.times+0.02
+    var i = this.data.item_distance + isPull* 1 / (x * x+1);
     this.setData({
+      startY: event.touches[0].pageY,
+      startX: event.touches[0].pageX,
       item_distance: i
     });
-    console.log(this.data.item_distance);
   },
   handletouchstart: function (event){
-    console.log(event)
     this.setData({
       touch :{
-        startX: event.touches[0].pageY,
-        startX: event.touches[0].pageX
+        startY: event.touches[0].pageY,
+        startX: event.touches[0].pageX,
+        times: 1.0
       }
     });
+  },
+  handletouchend: function(event){
+    var offset = this.data.item_distance - 20;
+    var i = offset / 300;
+    for(var x = 0;x < 300 ;x++){
+      var distance = this.data.item_distance - i;
+      this.setData({
+        item_distance: distance,
+        touch: {
+          startX: 0,
+          startX: 0,
+          times: 1.0
+        }
+      })
+    }
+    
   },
 
   /**
